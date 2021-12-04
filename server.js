a80269ce3c25c6cc70792506893e34f52e685c98
Carlene Cannon-Conner <carlenecannon-conner@carlenec-ukmac21.home>
2021-12-04 14:04:08 +0000

    Request/response validation and hooks

--- server.js
+++ server.js
@@ -2,8 +2,31 @@
 const fastify = require('fastify')({ logger: true })
 
 // declare a route
-fastify.get('/', async (req, reply) => {
-    return { hello: 'world' }
+fastify.route({
+    method: 'GET',
+    url: '/',
+    schema: {
+        // request needs to have a querystring with a `name` parameter
+        querystring: {
+            name: { type: 'string'}
+        },
+        // the response needs to be a `hello` object property of type string
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    hello: { type: 'string' }
+                }
+            }
+        }
+    },
+    // this function is executed for every request before the handler is executed
+    preHandler: async (req, reply) => {
+        // e.g. check authentication
+    },
+    handler: async (req, reply) => {
+        return { hello : 'world!' }
+    }
 })
 
 // run the server
